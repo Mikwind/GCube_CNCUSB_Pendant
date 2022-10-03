88fa875d82b0fe4e570eb1beeec50041a125ed5f
Mik Wind <em.miguel.s@gmail.com>
2022-10-03 20:35:24 +0200

    Map controller input to jog outputs

--- gamecube_pendant.ino/gamecube_pendant.ino.ino
+++ gamecube_pendant.ino/gamecube_pendant.ino.ino
@@ -1,24 +1,83 @@
 /*
+Code bases of  GamecubeController example by
   Copyright (c) 2014-2016 NicoHood
   See the readme for credit to other people.
 
-  GamecubeController example
-  Enter any key into the serial monitor to start reading.
-  Press A on the controller to enable rumble.
+  Additional code by Mik Wind
+*/
+
+/*
+  Map gamecube controller to a CNCUSB MK1 jog input.
+  Use D pad for X and Y axes
+  Use D Stick for Z and A axes.
+  R trigger to set the speed
+  See Defines for Pins
+
 */
 
 #include "Nintendo.h"
-
-// Define a Gamecube Controller
-CGamecubeController GamecubeController(7);
-
 // Pin definitions
 #define pinLed LED_BUILTIN
+#define pinController 7
+#define pinAnalogSpeedOut 2
+#define pinAminus 22
+#define pinAplus 23
+#define pinZminus 24
+#define pinZplus 25
+#define pinYminus 26
+#define pinYplus 27
+#define pinXminus 28
+#define pinXplus 29
+
+
+
+// Define a Gamecube Controller
+CGamecubeController GamecubeController(pinController);
+
+struct JogOutput
+{
+  uint8_t speed = 0;
+  bool x_plus = 0;
+  bool x_minus = 0;
+  bool y_plus = 0;
+  bool y_minus = 0;
+  bool z_plus = 0;
+  bool z_minus = 0;
+  bool a_plus = 0;
+  bool a_minus = 0;
+};
+
+void wtiteToJogOutput(const JogOutput& out)
+{
+  //Analog speed is inverted!:
+  analogWrite(pinAnalogSpeedOut, 255-out.speed);
+  digitalWrite(pinAminus, out.a_minus);
+  digitalWrite(pinAplus, out.a_plus);
+  digitalWrite(pinZminus, out.z_minus);
+  digitalWrite(pinZplus, out.z_plus);
+  digitalWrite(pinYminus, out.y_minus);
+  digitalWrite(pinYplus, out.y_plus);
+  digitalWrite(pinXminus, out.x_minus);
+  digitalWrite(pinXplus, out.x_plus);
+
+}
+
 
 void setup()
 {
-  // Set up debug led
+  // Set up PINS
   pinMode(pinLed, OUTPUT);
+  pinMode(pinAnalogSpeedOut, OUTPUT);
+  pinMode(pinLed, OUTPUT);
+  pinMode(pinAminus,OUTPUT);
+  pinMode(pinAplus,OUTPUT);
+  pinMode(pinZminus,OUTPUT);
+  pinMode(pinZplus,OUTPUT);
+  pinMode(pinYminus,OUTPUT);
+  pinMode(pinYplus,OUTPUT);
+  pinMode(pinXminus,OUTPUT);
+  pinMode(pinXplus,OUTPUT);
+
 
   // Start debug serial
   while (!Serial);
@@ -41,16 +100,17 @@ void loop()
     // Print Controller information
     auto status = GamecubeController.getStatus();
     auto report = GamecubeController.getReport();
+    convertGCtoJOgOut(report, status);
     print_gc_report(report, status);
-    delay(100);
+    delay(50);
 
     // Rumble if button "A" was pressed
-    if (report.a) {
-      GamecubeController.setRumble(true);
-    }
-    else {
-      GamecubeController.setRumble(false);
-    }
+    // if (report.a) {
+    //   GamecubeController.setRumble(true);
+    // }
+    // else {
+    //   GamecubeController.setRumble(false);
+    // }
   }
   else
   {
@@ -60,6 +120,22 @@ void loop()
     delay(1000);
   }
   digitalWrite(pinLed, LOW);
+}
+
+void convertGCtoJOgOut(Gamecube_Report_t &gc_report, Gamecube_Status_t &gc_status)
+{
+  static JogOutput theJogOutput;
+  theJogOutput.speed = gc_report.right-20;
+  theJogOutput.x_minus = gc_report.dleft;
+  theJogOutput.x_plus = gc_report.dright;
+  theJogOutput.y_minus = gc_report.dup;
+  theJogOutput.y_plus = gc_report.ddown;
+  theJogOutput.z_plus = (gc_report.cyAxis > 200);
+  theJogOutput.z_minus = (gc_report.cyAxis < 100);
+  theJogOutput.a_plus = (gc_report.cxAxis > 200);
+  theJogOutput.a_minus = (gc_report.cxAxis < 100);
+  wtiteToJogOutput(theJogOutput);
+
 }
 
 void print_gc_report(Gamecube_Report_t &gc_report, Gamecube_Status_t &gc_status)
@@ -124,18 +200,18 @@ void print_gc_report(Gamecube_Report_t &gc_report, Gamecube_Status_t &gc_status)
   // Serial.print(gc_report.z);
   // Serial.println(",");
 
-  // Serial.print(F("Dup:"));
-  // Serial.print(gc_report.dup);
-  // Serial.println(",");
-  // Serial.print(F("Ddown:"));
-  // Serial.print(gc_report.ddown);
-  // Serial.println(",");
-  // Serial.print(F("Dright:"));
-  // Serial.print(gc_report.dright);
-  // Serial.println(",");
-  // Serial.print(F("Dleft:"));
-  // Serial.print(gc_report.dleft);
-  // Serial.println(",");
+  Serial.print(F("Dup:"));
+  Serial.print(gc_report.dup);
+  Serial.println(",");
+  Serial.print(F("Ddown:"));
+  Serial.print(gc_report.ddown);
+  Serial.println(",");
+  Serial.print(F("Dright:"));
+  Serial.print(gc_report.dright);
+  Serial.println(",");
+  Serial.print(F("Dleft:"));
+  Serial.print(gc_report.dleft);
+  Serial.println(",");
 
   Serial.print(F("xAxis:"));
   Serial.print(gc_report.xAxis, DEC);
